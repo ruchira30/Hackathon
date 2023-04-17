28eae7b1a564461e65ea0291ae2e9ef88bdb721d
ruchira30 <108223104+ruchira30@users.noreply.github.com>
2023-04-18 01:35:35 +0530

    Update spaceshooters.py

--- spaceshooters.py
+++ spaceshooters.py
@@ -4,7 +4,11 @@ import sys
 
 # Initialize Pygame
 pygame.init()
-
+# Add sound
+pygame.mixer.init()
+sound = pygame.mixer.Sound("sound.mp3")
+sound.set_volume(0.2)
+sound.play()
 # Set up the game window
 window_width = 800
 window_height = 600
@@ -22,15 +26,15 @@ bullet_image = pygame.image.load("spr_bullet_strip02.png")
 player_rect = player_image.get_rect()
 player_rect.x = (window_width - player_rect.width) // 2
 player_rect.y = window_height - player_rect.height
-player_speed = 1.5
+player_speed = 2
 
 # Set up the bullets list
 bullets = []
-bullet_speed = 5
+bullet_speed = 1
 
 # Set up the enemies list
 enemies = []
-enemy_speed = 1
+enemy_speed = 0.8
 
 # Set up player lives
 player_lives = 5
@@ -43,12 +47,12 @@ button_width = 200
 button_height = 100
 button_x = (window_width - button_width) // 2
 button_y = (window_height - button_height) // 2
-start_button_image = pygame.image.load("play.png ")
+start_button_image = pygame.image.load("play.png")
 start_button_rect = start_button_image.get_rect()
-start_button_rect.center = (button_x + button_width // 2, button_y + button_height //2 )
+start_button_rect.center = (button_x + button_width // 2, button_y + button_height // 2)
 
 # Create a text surface
-text_surface = font.render("Space Shooters", True,(0,255,255))
+text_surface = font.render("Space Shooters", True, (0, 255, 255))
 
 # Get the dimensions of the text surface
 text_rect = text_surface.get_rect()
@@ -56,10 +60,12 @@ text_rect = text_surface.get_rect()
 # Center the text surface on the screen
 text_rect.center = (window_width // 2, 200)
 
+
+
 # Game loop
 show_start_screen = True
 while show_start_screen:
-    # Handle events
+# Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -68,7 +74,7 @@ while show_start_screen:
             # Check if the mouse was clicked inside the start button
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if start_button_rect.collidepoint(mouse_x, mouse_y):
-                # Start the game
+             # Start the game
                 show_start_screen = False
 
     # Draw graphics
@@ -90,9 +96,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             # Fire a bullet when the space bar is pressed
             bullet_rect = bullet_image.get_rect()
+
             bullet_rect.midbottom = player_rect.midtop
             bullets.append(bullet_rect)
 
@@ -124,7 +132,7 @@ while running:
     bullets = [bullet_rect for bullet_rect in bullets if bullet_rect.bottom >= 0]
 
     # Add enemies randomly
-    if len(enemies) < 10 and random.randint(0, 100) < 1:
+    if len(enemies) < 20 and random.randint(0, 100) < 1:
         enemy_rect = enemy_image.get_rect()
         enemy_rect.x = random.randint(0, window_width - enemy_rect.width)
         enemy_rect.y = -enemy_rect.height
@@ -156,13 +164,40 @@ while running:
         screen.blit(enemy_image, enemy_rect)
 
     # Draw lives
-    font = pygame.font.SysFont(None, 24)
+    font = pygame.font.SysFont(None, 30)
     lives_text = font.render("Lives: " + str(player_lives), True, (255, 255, 255))
     screen.blit(lives_text, (10, 10))
-
+    if player_lives <= 0:
+        # display game over message
+        font = pygame.font.SysFont(None, 50)
+        game_over_text = font.render("GAME OVER", True, (255, 0, 0))
+        game_over_rect = game_over_text.get_rect(center=(window_width / 2, window_height / 2))
+        screen.blit(game_over_text, game_over_rect)
+        pygame.display.flip()
+        pygame.time.wait(300)
     # Draw score
     score_text = font.render("Score: " + str(score), True, (255, 255, 255))
     screen.blit(score_text, (window_width - score_text.get_width() - 10, 10))
 
+    if score > 10:
+        font = pygame.font.SysFont("Arial", 50)
+        level_surface = font.render("Level 2", True, (255, 255, 255))
+        level_rect = level_surface.get_rect()
+        level_rect.center = (window_width // 2, 50)
+        screen.blit(level_surface, level_rect)
+
+    if score > 30:
+        font = pygame.font.SysFont("Arial", 50)
+        level_surface = font.render("Level 3", True, (255, 255, 255))
+        level_rect = level_surface.get_rect()
+        level_rect.center = (window_width // 2, 50)
+        screen.blit(level_surface, level_rect)
+
+        # Update the display again to show the change
+        pygame.display.flip()
+
     # Update the display
     pygame.display.update()
+
+
+
